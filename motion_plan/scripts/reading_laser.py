ef8608e8614f418691d61513d321d0b4c31d3369
JedtanutSem <94428679+JedtanutSem@users.noreply.github.com>
2021-11-16 12:29:58 +0700

    Add files via upload

---
+++ motion_plan/scripts/reading_laser.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+def clbk_laser(msg):
+    #720 / 5 = 144
+    regions = {
+    'right':min(min(msg.ranges[0:143]),10),
+    'fright':min(min(msg.ranges[144:287]),10),
+    'front':min(min(msg.ranges[288:431]),10),
+    'fleft':min(min(msg.ranges[432:575]),10),
+    'left':min(min(msg.ranges[576:713]),10)
+    }
+    rospy.loginfo(regions)
+    try:
+        regions = str(regions)
+        pub.publish(regions)
+    except:
+        rospy.loginfo("Error")
+    #talker_cira(regions)
+
+def main():
+    global pub
+    rospy.init_node('reading_laser')
+
+    sub = rospy.Subscriber('scan', LaserScan, clbk_laser)
+    pub = rospy.Publisher('toCira', String, queue_size=10)
+
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
