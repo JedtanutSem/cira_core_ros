3ac96293cf12734391728a189bd26df0e9d91c8b
JedtanutSem <94428679+JedtanutSem@users.noreply.github.com>
2021-11-16 12:42:02 +0700

    ino and cira core file

---
+++ Jedtanut_CiRA/Encoder_test/dummy_encoder_serial/dummy_encoder_serial.ino
@@ -0,0 +1,48 @@
+
+int i = 0;
+int x = 0;
+unsigned long current = 0;
+unsigned long last = 0;
+unsigned long diff = 0;
+void setup() {
+  Serial.begin(500000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+void loop()
+{
+  if(x == 0  )
+  {
+  for(i = 0 ; i<=10000;i++)
+  {
+    x++;
+    current = millis();
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(x+5);
+    delayMicroseconds(100);  
+    Serial.println("");
+    
+  }
+  if(i > 10000)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(5000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);
+  for(i = 0 ; i<=10000;i++)
+  {
+    x--;
+    current = millis();
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(x);
+    delayMicroseconds(100);  
+    Serial.println("");
+    
+  }
+  }
+  
+  }
+
+  }
