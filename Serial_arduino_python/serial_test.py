2eb0535dd8412f84336b6f5b1546d2f2208f57b4
JedtanutSem <94428679+JedtanutSem@users.noreply.github.com>
2021-12-14 04:16:58 -0500

    python communication

---
+++ Serial_arduino_python/serial_test.py
@@ -0,0 +1,27 @@
+import serial
+import time
+ser = serial.Serial(
+    port='/dev/ttyACM0',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+print("connected to: " + ser.portstr)
+
+#this will store the line
+line = []
+
+while True:
+    for c in ser.read():
+        line.append(c)
+        byte_bit = bin(c)
+        
+        print(int(byte_bit[7]))
+        print(byte_bit)
+        time.sleep(1)
+        
+        
+
+ser.close()
